ab2f317416cdbdc395f7016f7b2c430efa71d49d
Mke Norris <mike.norris@dcblox.com>
2020-11-19 21:08:32 -0500

    Final updates for 7.11.3

--- services/bonita-authentication/src/main/java/org/bonitasoft/engine/authentication/impl/AuthenticationServiceImpl.java
+++ services/bonita-authentication/src/main/java/org/bonitasoft/engine/authentication/impl/AuthenticationServiceImpl.java
@@ -13,16 +13,25 @@
  **/
 package org.bonitasoft.engine.authentication.impl;
 
+import java.io.IOException;
 import java.io.Serializable;
+import java.util.List;
 import java.util.Map;
 import java.util.concurrent.TimeUnit;
+
+import com.fasterxml.jackson.databind.ObjectMapper;
+
+import org.bonitasoft.engine.api.APIAccessor;
+import org.bonitasoft.engine.api.APIClient;
+import org.bonitasoft.engine.api.IdentityAPI;
+import org.bonitasoft.engine.api.LoginAPI;
+import org.bonitasoft.engine.authentication.impl.model.*;
+import org.bonitasoft.engine.exception.AlreadyExistsException;
+import org.bonitasoft.engine.identity.*;
+import org.bonitasoft.engine.session.APISession;
 import org.bonitasoft.engine.authentication.AuthenticationConstants;
 import org.bonitasoft.engine.authentication.GenericAuthenticationService;
-import org.bonitasoft.engine.authentication.impl.model.*;
 import org.bonitasoft.engine.commons.LogUtil;
-import org.bonitasoft.engine.identity.IdentityService;
-import org.bonitasoft.engine.identity.SUserNotFoundException;
-import org.bonitasoft.engine.identity.User;
 import org.bonitasoft.engine.identity.model.SUser;
 import org.bonitasoft.engine.log.technical.TechnicalLogSeverity;
 import org.bonitasoft.engine.log.technical.TechnicalLoggerService;
@@ -57,13 +66,14 @@ public class AuthenticationServiceImpl implements GenericAuthenticationService {
     private int lastLdapUrlIndex;
     private final String domainName = "MYDCBLOX.COM";
     private Attributes attrs;
-    protected BonitaRestAPIService bonitaRestAPIService;
+    private static final ObjectMapper mapper = new ObjectMapper();
+    private IdentityAPI identityAPI;
+    private LoginAPI loginAPI;
 
-    public AuthenticationServiceImpl(final IdentityService identityService, BonitaRestAPIService bonitaRestAPIService, final TechnicalLoggerService logger) {
+    public AuthenticationServiceImpl(final IdentityService identityService, final TechnicalLoggerService logger) {
         System.out.println("AuthenticationServiceImpl:::JAR CONSTRUCTED!!!");
         this.identityService = identityService;
         this.logger = logger;
-        this.bonitaRestAPIService = bonitaRestAPIService;
 
         lastLdapUrlIndex = 0;
     }
@@ -123,8 +133,6 @@ public class AuthenticationServiceImpl implements GenericAuthenticationService {
                             String firstName = attrs.get("givenName").get().toString();
                             String lastName = attrs.get("sn").get().toString();
 
-                            //UserIdentificationResponse userResponse = bonitaRestAPIService.getUserIdentificationResponse(sessionInfo);
-
                             System.out.println("Creating user:"+userName);
                             UserCreateRequest userCreateRequest = new UserCreateRequest();
                             userCreateRequest.setUserName(userName);
@@ -134,11 +142,10 @@ public class AuthenticationServiceImpl implements GenericAuthenticationService {
                             userCreateRequest.setPassword(userPass+"_bonitapass");
                             userCreateRequest.setPassword_confirm(userPass+"_bonitapass");
 
-                            User userDetails = bonitaRestAPIService.createUser(userCreateRequest);
+                            User userDetails = this.createUser(userCreateRequest);
                             if (null == userDetails) {
                                 return "";
                             }
-                            //String userDetails = bonitaRestAPIService.createUser(userCreateRequest, sessionInfo);
                             System.out.println("Posted user:"+userName+" to bonita");
                             System.out.println("CREATE USER RESONPONSE:"+userDetails);
 
@@ -155,7 +162,7 @@ public class AuthenticationServiceImpl implements GenericAuthenticationService {
 
                                 try {
                                     System.out.println("Posting new membership with userid:" + userID);
-                                    bonitaRestAPIService.addMembership(membership);
+                                    this.addMembership(membership);
                                     System.out.println("PostedNewMembership for userid:" + userID);
                                 } catch (Exception e) {
                                     System.out.println(e.getMessage());
@@ -166,18 +173,18 @@ public class AuthenticationServiceImpl implements GenericAuthenticationService {
 
                                 try {
                                     System.out.println("Posting professional details email:" + email);
-                                    bonitaRestAPIService.createProfessionalContactDetails(bonitaUserContactDetails);
+                                    this.createProfessionalContactDetails(bonitaUserContactDetails);
                                     System.out.println("Response status from posting email:" + email + " for user name:" + userDetails.getUserName());
                                 } catch (Exception e) {
                                     System.out.println("Creating professional contact detailed failed. Will attempt update instead.");
                                     e.printStackTrace();
                                     System.out.println("Updating professional details email:" + email);
-                                    bonitaRestAPIService.updateProfessionalContactDetails(bonitaUserContactDetails);
+                                    this.updateProfessionalContactDetails(bonitaUserContactDetails);
                                     System.out.println("Response status from updating email:" + email + " for user name:" + userDetails.getUserName());
                                 }
 
                                 System.out.println("Posting guid:"+guid);
-                                bonitaRestAPIService.createGUID("{\"value\":\"" + guid + "\"}", userID);
+                                this.createGUID("{\"value\":\"" + guid + "\"}", userID);
                                 System.out.println("Putting the guid:"+guid);
 
                             } else {
@@ -200,11 +207,11 @@ public class AuthenticationServiceImpl implements GenericAuthenticationService {
                     bonitaUserContactDetails.setId(user.getId()+"");
 
                     System.out.println("Posting professional details email:"+email+" for already created user");
-                    bonitaRestAPIService.updateProfessionalContactDetails(bonitaUserContactDetails);
+                    this.updateProfessionalContactDetails(bonitaUserContactDetails);
                     System.out.println("Response status from posting email:"+email+" for user name:"+user.getUserName());
 
                     System.out.println("Posting guid:"+guid+" for already created user");
-                    bonitaRestAPIService.createGUID(guid, user.getId()+"");
+                    this.createGUID(guid, user.getId()+"");
                     System.out.println("Putting the guid:"+guid);
 
                 } catch (Throwable throwable) {
@@ -332,5 +339,154 @@ public class AuthenticationServiceImpl implements GenericAuthenticationService {
         } else {
             return Integer.toHexString(value);
         }
+    }
+
+    public boolean initialize() {
+        APIClient apiClient = new APIClient();
+        try {
+            apiClient.login("install", "install");
+            this.identityAPI = apiClient.getIdentityAPI();
+        } catch (Exception e) {
+            return false;
+        }
+        return true;
+    }
+
+    public void addMembership(Membership membership) throws Exception {
+        Boolean init = this.initialize();
+        Long userId = Long.valueOf(membership.getUser_id());
+        Long groupId = Long.valueOf(membership.getGroup_id());
+        Long roleId = Long.valueOf(membership.getRole_id());
+        if (init) {
+            try {
+                UserMembership userMembership = identityAPI.addUserMembership(userId, groupId, roleId);
+            } catch (Exception e) {
+                System.out.format("Could not add user %s to group %s with role %s", userId, groupId, roleId);
+                e.printStackTrace();
+                throw new Exception(e.getMessage());
+            }
+        } else {
+            System.out.format("Could not connect with install install");
+        }
+    }
+
+    public BonitaUserCustomDetail createGUID(String guid, String userId) throws Exception {
+        Boolean init = this.initialize();
+        Long luserId = Long.valueOf(userId);
+        if (init) {
+            try {
+                List<CustomUserInfoDefinition> defs = identityAPI.getCustomUserInfoDefinitions(0,100);
+                for (CustomUserInfoDefinition def : defs) {
+                    if (def.getName().toLowerCase().contains("guid")) {
+                        identityAPI.setCustomUserInfoValue(def.getId(), luserId, guid);
+                    }
+                }
+            } catch (Exception e) {
+                System.out.format("Could not add GUID %s to user %s", guid, userId);
+                e.printStackTrace();
+                throw new Exception(e.getMessage());
+            }
+        } else {
+            System.out.format("Could not connect with install install");
+        }
+        return null;
+    }
+
+
+    public APISession loginUser(String userName, String password) {
+        APIClient apiClient = new APIClient();
+        try {
+            apiClient.login(userName, password);
+            APISession session = loginAPI.login(userName, password);
+            Long tenant = session.getTenantId();
+            System.out.format("Logged user %s in with %s and tenant set to %s", userName, password, tenant);
+            return session;
+        } catch (Exception e) {
+            System.out.format("Could not login as user %s", userName);
+        }
+        return null;
+    }
+
+    public UserWithContactData createProfessionalContactDetails(BonitaUserContactDetails contactDetails) {
+        Boolean init = this.initialize();
+        if (init) {
+            Long userId = Long.valueOf(contactDetails.getId());
+            try {
+                UserWithContactData user = identityAPI.getUserWithProfessionalDetails(userId);
+                ContactData contactData = user.getContactData();
+                ContactDataUpdater proContactUpdater = new ContactDataUpdater();
+                proContactUpdater.setEmail(contactDetails.getEmail());
+                UserUpdater updater = new UserUpdater();
+                updater.setEnabled(true);
+                updater.setProfessionalContactData(proContactUpdater);
+                identityAPI.updateUser(userId, updater);
+                return user;
+            } catch (Exception e) {
+                System.out.format("Could not get data for user %s <%s>", contactDetails.getId(), contactDetails.getEmail());
+            }
+        }
+        return null;
+    }
+
+    public UserWithContactData updateProfessionalContactDetails(BonitaUserContactDetails contactDetails) {
+        Boolean init = this.initialize();
+        if (init) {
+            Long userId = Long.valueOf(contactDetails.getId());
+            try {
+                UserWithContactData user = identityAPI.getUserWithProfessionalDetails(userId);
+                ContactData contactData = user.getContactData();
+                ContactDataUpdater perContactUpdater = new ContactDataUpdater();
+                perContactUpdater.setEmail(contactDetails.getEmail());
+                UserUpdater updater = new UserUpdater();
+                updater.setEnabled(true);
+                updater.setPersonalContactData(perContactUpdater);
+                identityAPI.updateUser(userId, updater);
+                return user;
+            } catch (Exception e) {
+                System.out.format("Could not get data for user %s <%s>", contactDetails.getId(), contactDetails.getEmail());
+            }
+        }
+        return null;
+    }
+
+    public UserWithContactData  getUserIdentificationResponse (String userid) {
+        Boolean init = this.initialize();
+        if (init) {
+            Long userId = Long.valueOf(userid);
+            try {
+                return identityAPI.getUserWithProfessionalDetails(userId);
+            } catch (Exception e) {
+                System.out.format("Could not get data for user %s", userid);
+            }
+        }
+        return null;
+    }
+
+    public User createUser(UserCreateRequest userCreateRequest) {
+        Boolean init = this.initialize();
+        if (init) {
+            UserCreator creator = new UserCreator(userCreateRequest.getUserName(), userCreateRequest.getPassword());
+            creator.setFirstName(userCreateRequest.getFirstname())
+                    .setLastName(userCreateRequest.getLastname())
+                    .setEnabled(true);
+            try {
+                return identityAPI.createUser(creator);
+            } catch (AlreadyExistsException e) {
+                System.out.format("Could not create user %s as it already exists", userCreateRequest.getUserName());
+            } catch (Exception e) {
+                System.out.format("Could not create user %s", userCreateRequest.getUserName());
+            }
+        }
+        return null;
+    }
+
+    private static UserIdentificationResponse mapUserIdentification(String responseString) {
+        UserIdentificationResponse response = new UserIdentificationResponse();
+        try {
+            response = mapper.readValue(responseString, UserIdentificationResponse.class);
+        } catch (IOException e) {
+            response.setError("Error "+e.toString());
+        }
+        return response;
     }
 }
